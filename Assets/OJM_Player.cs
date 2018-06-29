15ee74e7a4191f2142a15d87472f52d3d9d0c8f5
iuyhcdfs <iuyhcdfs@gmail.com>
2018-06-29 21:19:32 +1000

    todo, movement, camera follow, kaizen

---
+++ Assets/OJM_Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OJM_Player : MonoBehaviour {
+
+    /*
+    jumping and movement
+    sweep test for ludicrous speed
+    decent turning with the analog stick conversion
+    trigger reaction for touching the ground and being able to jump
+    trigger reaction for 
+    */
+
+    private float BaseRun = 5;
+    private float BaseJump = 4;
+
+    private void OnCollisionEnter(Collision collision) {
+        
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+}
